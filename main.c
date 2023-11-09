7356510dc5411638da4307eea77b559f22ccdbc7
Müller Barnabás <mbarni458@gmail.com>
2023-11-09 18:53:35 +0100

    Import libraries

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 #include <time.h>
 #ifdef _WIN32
     #include <windows.h>
+#include <conio.h>
+
 #endif
 
 #include "debugmalloc.h"
